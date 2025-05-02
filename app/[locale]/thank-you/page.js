8c5f7abf954062636a3e57981f06b132139188f4
valenWiselink <valentin.mathieur@wiselink.app>
2025-05-02 10:55:45 -0300

    Eliminar CalendlySection de la página de agradecimiento

--- app/[locale]/thank-you/page.js
+++ app/[locale]/thank-you/page.js
@@ -1,13 +1,11 @@
 "use client";
 
 import ThankYouSection from "@/components/templates/ThankYouSection";
-import CalendlySection from "@/components/templates/landing/calendly/CalendlySection";
 
 const page = () => {
   return (
     <main>
       <ThankYouSection />
-      <CalendlySection />
     </main>
   );
 };
